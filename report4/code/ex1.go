7574f58638f9143f62058c33fd9ff5d9cb0ece94
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2020-06-04 10:23:58 +0900

    add ex1

--- report4/code/ex1.go
+++ report4/code/ex1.go
@@ -11,14 +11,31 @@ import (
 
 var job_time = [4][4]int{{6, 1, 9, 3}, {2, 5, 7, 8}, {6, 3, 5, 4}, {3, 5, 2, 1}}
 
-func mslsearch(c []int) int{
-	first_value := job_time[0][c[0] - 1] + job_time[1][c[1] - 1] + job_time[2][c[2] - 1] + job_time[3][c[3] - 1]
-	return kotae
+func swap(c []int, i int) []int{
+	swap_c := make([]int, len(c))
+	_ = copy(swap_c, c)
+	swap_c[i], swap_c[i+1] =  swap_c[i+1], swap_c[i]
+	return swap_c
+}
+
+func mslsearch(comb []int) (int, int){
+	first_value := job_time[0][comb[0]-1] + job_time[1][comb[1]-1] + job_time[2][comb[2]-1] + job_time[3][comb[3]-1]
+
+	result_value := first_value
+	for i := 0; i < len(comb) - 1; i++ {
+		swap_comb := swap(comb, i)
+		new_value := job_time[0][swap_comb[0] - 1] + job_time[1][swap_comb[1] - 1] + job_time[2][swap_comb[2] - 1] + job_time[3][swap_comb[3] - 1]
+		if result_value > new_value {
+			result_value = new_value
+		}
+	}
+	return first_value, result_value
 }
 
 func main(){
 
 	comb := []int{3, 4, 1, 2}
-	result := mslsearch(comb)
-	fmt.Println(result)
+	first, result := mslsearch(comb)
+	fmt.Println("初期解", first)
+	fmt.Println("最良解", result)
 }
