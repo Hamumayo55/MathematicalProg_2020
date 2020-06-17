a4d1ae4ade6dfb2a20c6048cfbf3bc70c3169c66
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2020-06-17 11:17:25 +0900

    修正

--- report4/code/ex4.go
+++ report4/code/ex4.go
@@ -45,7 +45,7 @@ func shuffle(data []int) []int{
 	n := len(data)
     for i := n-1; i >= 0; i-- {
         j := rand.Intn(i+1)
-        data[i], data[j] = data[j], data[i]
+		data[i], data[j] = data[j], data[i]
 	}
 	return data
 }
