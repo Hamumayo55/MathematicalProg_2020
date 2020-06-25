8bcb6ffa1d9baae5bbf76a29536ccf99d8496c8f
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2020-06-25 20:29:58 +0900

    matrixの追加

--- report5/code/ex1.go
+++ report5/code/ex1.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main(){
-	matrix := [][]float64{{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}}
-	fmt.Println(matrix)
+	matrix := [][]float64{{0,0,0,0},{1,0,0,0},{0,0,0,0},{0,0,0,0}}
+	fmt.Println(matrix[1][0])
 }
